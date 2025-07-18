c7b69b1cab7dfb182104f8cc1730e3464ac1d6e1
shiwan-mangate <shiwanmangate1234@gmail.com>
2025-07-18 20:02:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,81 @@
+import streamlit as st
+import pickle
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+import nltk
+import string
+import nltk
+from nltk.tokenize import word_tokenize
+
+import nltk
+import os
+
+# Download NLTK resources at runtime if not already present
+nltk_data_path = os.path.join(os.getcwd(), 'nltk_data')
+if not os.path.exists(nltk_data_path):
+    os.makedirs(nltk_data_path)
+
+nltk.data.path.append(nltk_data_path)
+
+nltk.download('stopwords', download_dir=nltk_data_path)
+nltk.download('punkt', download_dir=nltk_data_path)
+
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words('english'))
+
+
+nltk.download('stopwords')
+nltk.download('punkt')  # Needed for word_tokenize
+
+
+
+sn = SnowballStemmer("english")
+
+
+
+def transform_text (text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y =[]
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text :
+        y.append(sn.stem(i))
+
+
+
+    return " ".join(y)
+
+tfidf = pickle.load(open("vectorizer.pkl","rb"))
+model = pickle.load(open("model.pkl","rb"))
+
+st.title("SMS SPAM CLASSIFIER")
+
+input_sms = st.text_input("Enter the message")
+
+if st.button("Predict"):
+# preprocess
+  transformed_sms = transform_text(input_sms)
+# vectorize
+  vector_input = tfidf.transform([transformed_sms])
+# predict
+  result = model.predict(vector_input)[0]
+
+# Display
+  if result == 1:
+    st.header("Spam")
+  else:
+     st.header("NOT spam")
